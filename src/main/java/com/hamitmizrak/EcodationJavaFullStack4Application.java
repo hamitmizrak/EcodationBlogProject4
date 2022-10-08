ae02cf6e2e4b802159fd8773f6ff4dfa80f0cbc7
hamitmizrak <hamitmizrak@gmail.com>
2022-10-08 18:43:06 +0300

    BCrypted

--- src/main/java/com/hamitmizrak/EcodationJavaFullStack4Application.java
+++ src/main/java/com/hamitmizrak/EcodationJavaFullStack4Application.java
@@ -2,6 +2,7 @@ package com.hamitmizrak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -22,15 +23,14 @@ import org.springframework.scheduling.annotation.EnableAsync;
 //Auditor
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareMethod")
 
-
 //exclude:dahil etmemek
-/*@SpringBootApplication(exclude = {
+@SpringBootApplication(exclude = {
         SecurityAutoConfiguration.class,
         //org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
         //org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
 }
-)*/
-@SpringBootApplication
+)
+//@SpringBootApplication
 public class EcodationJavaFullStack4Application {
 
     public static void main(String[] args) {
