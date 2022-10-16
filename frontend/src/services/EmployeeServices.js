e73892879921b9c66ccca4b728ad917c3bb9815e
hamitmizrak <hamitmizrak@gmail.com>
2022-10-16 16:22:03 +0300

    otherLanguageServices

--- frontend/src/services/EmployeeServices.js
+++ frontend/src/services/EmployeeServices.js
@@ -5,16 +5,23 @@ import axios from "axios";
 
 //sabit URL
 //http://localhost:8080/employee/api/v1/employees
-const EMPLOYEE_URL="/employee/api/v1/employees";
+const EMPLOYEE_URL = "/employee/api/v1/employees";
 
 //class
-class EmployeeServices{
+class EmployeeServices {
+
+
+    //Header Flags(i18n data)
+    otherLanguageServices(language) {
+        axios.defaults.headers['accept-language'] = language;
+    }
+
 
     //SAVE 
     //POST =>  http://localhost:8080/employee/api/v1/employees
     createEmployee(employeeDto) {
         //URL,BODY
-       return axios.post(EMPLOYEE_URL,employeeDto);
+        return axios.post(EMPLOYEE_URL, employeeDto);
     }
 
     //LIST
@@ -26,19 +33,19 @@ class EmployeeServices{
     //FIND
     //GET => http://localhost:8080/employee/api/v1/employees/1
     getEmployeeById(id) {
-        return axios.get(EMPLOYEE_URL+"/"+id);
+        return axios.get(EMPLOYEE_URL + "/" + id);
     }
 
     //DELETE
     //DELETE => http://localhost:8080/employee/api/v1/employees/1
     deleteEmployee(id) {
-        return axios.delete(EMPLOYEE_URL+"/"+id);
+        return axios.delete(EMPLOYEE_URL + "/" + id);
     }
 
     //UPDATE
     //PUT => http://localhost:8080/employee/api/v1/employees/1
-     updateEmployee(id,employeeDto) {
-        return axios.put(EMPLOYEE_URL+"/"+id,employeeDto);
+    updateEmployee(id, employeeDto) {
+        return axios.put(EMPLOYEE_URL + "/" + id, employeeDto);
     }
 } //end EmployeeServices
 
